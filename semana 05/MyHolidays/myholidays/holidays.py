4083e53c17a0b8856d2d3a275060d12804d137d6
GuilhermeSSx <guilhermedosantos45@gmail.com>
2021-06-13 04:57:35 -0300

    exercicios feitos semana 05-06-07-08

--- semana 05/MyHolidays/myholidays/holidays.py
+++ semana 05/MyHolidays/myholidays/holidays.py
@@ -1,8 +1,25 @@
-from datetime import date
+from datetime import date, datetime
 
 class MyCalendar:
-    def __init__(self, *args):
-        self.datas = []
-        self.datas.append(date(2021, 12, 5))
-        # self.check_holiday = 
-        # self.add_holiday = 
+	def __init__(self, *args):
+		self.datas = []
+		self.add_holiday(*args)
+
+	def validar_data(self, data):
+		if isinstance(data, date):
+			return data
+		elif isinstance(data, str):
+			try:
+				return datetime.strptime(data, '%d/%m/%Y').date()
+			except:
+				return None
+
+	def add_holiday(self, *args):
+		for data in args:
+			data_arg = self.validar_data(data)
+			if data_arg:
+				self.datas.append(data_arg)
+		self.datas = list(set(self.datas))
+
+	def check_holiday(self, data):
+		return self.validar_data(data) in self.datas
--- semana 08/rh/classes/Departamento.py
+++ semana 08/rh/classes/Departamento.py
@@ -1,12 +1,28 @@
 from rh.classes.Colaborador import Colaborador
+from datetime import date
 
 
 class Departamento:
-    def __init__(self, nome_setor):
+    def __init__(
+        self,
+        nome_setor,
+        nome_responsavel=None,
+        dia=None,
+        mes=None,
+        ano=None
+    ):
         self._nome_setor = nome_setor
-        self._responsavel = None
+        self._responsavel = nome_responsavel
         self._colaboradores = []
+        self._hoje = date.today()
 
+        try:
+            self._aniversario_resp = date(ano, mes, dia)
+        except TypeError:
+            raise TypeError('Informe dia, mês e ano')
+    @property
+    def aniversario_resp(self):
+        return self._aniversario_resp.isoformat()
     @property
     def nome(self):
         return self._nome_setor
@@ -31,15 +47,31 @@ class Departamento:
     def add_colaborador(self, nome, dia, mes, ano):
         self._colaboradores.append(Colaborador(nome, dia, mes, ano))
 
+    def aniversario_hoje(self):
+        if Colaborador._aniversario.day == self._hoje.day:
+            if Colaborador._aniversario.month == self._hoje.month:
+                return True
+        return False
+
     def verificar_aniversariantes(self):
         lista = []
         for colaborador in self.colaboradores:
             if colaborador.aniversario_hoje():
-                lista.append((colaborador.nome, colaborador.aniversario))
+                lista.append((
+                    colaborador.nome,
+                    colaborador.aniversario,
+                    self.nome))
+        if Departamento.responsavel:
+            if self._aniversario_resp.day == self._hoje.day:
+                if self._aniversario_resp.month == self._hoje.month:
+                    lista.append((
+                        self.responsavel,
+                        self.aniversario_resp,
+                        self.nome))
         return lista
 
     def __str__(self):
         return self._nome_setor
 
     def __repr__(self):
-        return 'Departamento = ' + self._nome_setor
+        return 'Departamento = ' + self._nome_setor 
